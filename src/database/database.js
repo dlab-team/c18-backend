49c985b50f09f0b883a284b733b4fd1bc83e31f6
Felipe <39388875+lfazocar@users.noreply.github.com>
2024-05-20 02:37:10 -0400

    feat: add PGPORT env variable, add .env to npm run dev script

--- src/database/database.js
+++ src/database/database.js
@@ -6,6 +6,7 @@ export const sequelize = new Sequelize(
   process.env.PGPASSWORD || "1234",
   {
     host: process.env.PGHOST || "localhost",
+    port: process.env.PGPORT || "5432",
     dialect: "postgres",
   }
 );
