59dabef2734ca091315a2a95178302dfcfd51160
Felipe <39388875+lfazocar@users.noreply.github.com>
2024-05-20 18:20:29 -0400

    feat: add dotenv to manage .env variables, rewrite sequelize database config
    config.json is now database.cjs and uses environment variables to hide secrets
    sequelize now uses different database settings depending on environment (dev, test, prod)
    added authentication step to sequelize database connection to catch errors

---
+++ src/config/database.cjs
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres',
+    migrationStorageTableName: 'migrations'
+  },
+  test: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE + '_test',
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres',
+    migrationStorageTableName: 'migrations'
+  },
+  production: {
+    url: process.env.PGSTRING,
+    dialect: 'postgres',
+    migrationStorageTableName: 'migrations'
+  }
+};
