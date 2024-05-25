8762ae96779cf33d4105b4a10349453f214c53c5
manu4970 <manuelperez497078@gmail.com>
2024-05-25 14:21:02 -0400

    update: Se actualza variables de entorno en server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import swaggerDocs from "./docs/swagger.cjs";
 import routes from "./routes/index.js";
-import swaggerDocs from "./v1/swagger.cjs";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000;
 
 // rutas importadas
 routes(app);
@@ -12,7 +14,8 @@ routes(app);
 app.get("/", (req, res) => {
   res.send("c18 API");
 });
+
 app.listen(3000, () => {
-  console.log("Server running on port http://localhost:3000");
-  swaggerDocs(app, 3000);
+  console.log(`Server running on port http://localhost:${port}`);
+  swaggerDocs(app, port);
 });
