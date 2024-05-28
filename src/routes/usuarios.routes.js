1395beabae0fb8c611ae81ae20adaae6e87a6fc6
manu4970 <manuelperez497078@gmail.com>
2024-05-27 20:51:08 -0400

    update: Se agrega auth a rutas usuarios

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -7,13 +7,35 @@ import {
   putUsuariosPorId,
 } from "../controllers/usuariosController.js";
 
+import { requiereAuth, rolAccess } from "../middleware/auth.js";
+
 const router = Router();
 
 // Rutas para los usuarios
-router.get("/usuarios", getUsuarios);
+router.get(
+  "/usuarios",
+  requiereAuth,
+  rolAccess({ roles: ["admin"] }),
+  getUsuarios
+);
 router.post("/usuarios", postUsuarios);
-router.get("/usuarios/:id", getUsuariosPorId);
-router.put("/usuarios/:id", putUsuariosPorId);
-router.delete("/usuarios/:id", deleteUsuariosPorId);
+router.get(
+  "/usuarios/:id",
+  requiereAuth,
+  rolAccess({ roles: ["admin"] }),
+  getUsuariosPorId
+);
+router.put(
+  "/usuarios/:id",
+  requiereAuth,
+  rolAccess({ roles: ["admin"] }),
+  putUsuariosPorId
+);
+router.delete(
+  "/usuarios/:id",
+  requiereAuth,
+  rolAccess({ roles: ["admin"] }),
+  deleteUsuariosPorId
+);
 
 export default router;
