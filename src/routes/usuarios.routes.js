7c16d29bfec58d44bbf371d4541c7803d9a7691d
Felipe <lfazocar@users.noreply.github.com>
2024-05-30 19:54:50 -0400

    fix: routes middleware

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -13,26 +13,26 @@ const router = Router();
 
 // Rutas para los usuarios
 router.get(
-  "/usuarios",
+  "/",
   requiereAuth,
   rolAccess({ roles: ["admin"] }),
   getUsuarios
 );
-router.post("/usuarios", postUsuarios);
+router.post("/", postUsuarios);
 router.get(
-  "/usuarios/:id",
+  "/:id",
   requiereAuth,
   rolAccess({ roles: ["admin"] }),
   getUsuariosPorId
 );
 router.put(
-  "/usuarios/:id",
+  "/:id",
   requiereAuth,
   rolAccess({ roles: ["admin"] }),
   putUsuariosPorId
 );
 router.delete(
-  "/usuarios/:id",
+  "/:id",
   requiereAuth,
   rolAccess({ roles: ["admin"] }),
   deleteUsuariosPorId
